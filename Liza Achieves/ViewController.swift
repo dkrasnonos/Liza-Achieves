6626e9630321c9129031b49d1d38756259ad12c9
dkrasnonos <krasnonos.d@gmail.com>
2023-01-21 08:47:55 +0400

    Initial

---
+++ Liza Achieves/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Liza Achieves
+//
+//  Created by Дмитрий Краснонос on 21.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
